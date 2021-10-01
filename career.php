f05ea7183e934a8432a2f8fa0d915e1ccc68b015
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-01 17:54:59 +0530

    Create career.php

---
+++ career.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Career</title>
+</head>
+
+<body>
+    <div style="background-color:#333; color:#fff;">
+        <div style="padding-top:10px; padding-bottom:10px; padding-left:20px;">
+            <h1>Job Portal</h1>
+            <p>Best jobs available matching your skills.</p>
+        </div>
+    </div>
+    </div>
+
+    <div class="container-fluid" style="margin-top:20px;">
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h4 class="card-title">Web Dev</h4>
+    <h5 class="card-title">Company Name</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"><h6 class="card-title">Skills Required: </h6>HTML, CSS, PHP</li>
+    <li class="list-group-item"><h6 class="card-title">Job Location: </h6>Delhi, Pune</li>
+    <li class="list-group-item"><h6 class="card-title">CTC: </h6>3.5 LPA</li>
+  </ul>
+</div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
