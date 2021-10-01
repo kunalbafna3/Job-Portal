dc230e92df71f95de3f0b61aa5273fdbf46330cd
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-01 15:10:41 +0530

    Update register.php

--- register.php
+++ register.php
@@ -6,10 +6,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <style>
+        body{
+            background-image: url("img.png");
+            background-size: cover;
+        }
+        form {
+            background-color: #fff; 
+            margin: 3em 10em 3em 30em;
+            padding: 30px;
+            box-shadow: 10px 10px 8px 10px #888888;
+        }
+    </style>
+
     <title>Register</title>
 </head>
 
 <body>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputName" class="form-label">Full Name</label>
+                <input type="text" class="form-control" id="exampleInputName">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputNumber" class="form-label">Phone Number</label>
+                <input type="number" class="form-control" id="exampleInputNumber" >
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword2">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <br>
+            Already Registered? <a href="Login.php">Login</a>
+        </form>
+    </div>
+
 </body>
 
 </html>
