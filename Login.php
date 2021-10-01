ce1f483a3e2be210d60247719c4e64d0aa6d105e
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-01 15:10:09 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -6,11 +6,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <style>
+        body{
+            background-image: url("img.png");
+            background-size: cover;
+        }
+        form {
+            background-color: #fff; 
+            margin: 3em 10em 3em 30em;
+            padding: 30px;
+            box-shadow: 10px 10px 8px 10px #888888;
+        }
+    </style>
 
     <title>Login</title>
 </head>
 
 <body>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <p style="text-align: center;">New User?<br>Create Account <a href="register.php">Sign Up</a></p>
+        </form>
+    </div>
+
 </body>
 
 </html>
