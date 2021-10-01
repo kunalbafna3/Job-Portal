0f093eb4d644c19d7100651e11dede1e0e43c123
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-01 17:55:07 +0530

    Create jobs.php

---
+++ jobs.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'?>
+
+<div class="content">
+    <table class="table">
+        <br><br><br>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Candidate Name</th>
+                <th scope="col">Position</th>
+                <th scope="col">Resume</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Kunal Bafna</td>
+                <td>Web Dev</td>
+                <td><a href=""><i class="fa fa-download" aria-hidden="true"></i></a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
