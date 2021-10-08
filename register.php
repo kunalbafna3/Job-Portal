57377288a8652e2741e5b94993d665771774de76
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-09 01:14:28 +0530

    Update register.php

--- register.php
+++ register.php
@@ -28,29 +28,29 @@
 
 <body>
     <div class="container">
-        <form>
+        <form action="" method="POST">
             <div class="mb-3">
                 <label for="exampleInputName" class="form-label">Full Name</label>
-                <input type="text" class="form-control" id="exampleInputName">
+                <input type="text" class="form-control" id="exampleInputName" name="name">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputNumber" class="form-label">Phone Number</label>
-                <input type="number" class="form-control" id="exampleInputNumber" >
+                <input type="number" class="form-control" id="exampleInputNumber" name="phone_no" >
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword2">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
             <br>
             Already Registered? <a href="Login.php">Login</a>
         </form>
