99c772ef150fc60d86ee6b788c3fa80d9dff1eaa
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-09 18:47:21 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,3 +1,4 @@
+<?php include 'config.php';?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,16 +29,16 @@
 
 <body>
     <div class="container">
-        <form>
+        <form method="POST">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" name="Login">Submit</button>
             <p style="text-align: center;">New User?<br>Create Account <a href="register.php">Sign Up</a></p>
         </form>
     </div>
