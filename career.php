94b5b5b3b5efb5fdf46a9a9d28f6bef3ac7a433f
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-10 14:55:30 +0530

    Update career.php

--- career.php
+++ career.php
@@ -1,3 +1,4 @@
+<?php include 'config.php'?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +11,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
+        <style>
+            .borderr{
+                border: 1px solid black;
+                margin: 25px;
+                padding: 5px;
+            }
+        </style>
     <title>Career</title>
 </head>
 
@@ -25,20 +33,31 @@
     <div class="row">
     <?php
 
-$sql="SELECT `cname`, `position`, `Jdesc`,`skills`,`CTC` FROM 'jobs'";
+$sql="SELECT `cname`, `position`, `Jdesc`,`skills`,`CTC` FROM `jobs`";
 $result = mysqli_query($conn,$sql);
-$i=0;
-if($result->num_rows>0){
+
+if ($result->num_rows > 0){
     // output data of each row
-    while($row=$result->fetch_assoc()){ 
-       
-}} else{
-    echo "";
-}
+    while($rows=$result->fetch_assoc()){ 
+       echo '
+       <div class="col-md-4 borderr">
+       <div class="jobs">
+       <h3 style="text-align:center;">'.$rows['position'].'</h3>
+       <h4 style="text-align:center;">'.$rows['cname'].'</h4>
+       <p style="color:black; text-align:justify;">'.$rows['Jdesc'].'</p>
+       <p style="color:black;"><b>skills required: </b>'.$rows['skills'].'</p>
+       <p style="color:black;"><b>CTC: </b>'.$rows['CTC'].'</p>
+       <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModal" role="button" aria-expanded="false"
+       aria-controls="collapseExample">
+       Apply Now
+        </a>
+            </div>
+        </div>';
+}}
 ?>
     </div>
-
-
+  </div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
