1efbd8e44c9af8c53e65c846557b71599cb83b55
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-10 14:55:34 +0530

    Update career.php

--- career.php
+++ career.php
@@ -55,6 +55,38 @@ if ($result->num_rows > 0){
         </div>';
 }}
 ?>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apply for Jobs</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="POST">
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Name:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Applying For:</label>
+            <input type="text" class="form-control" name="apply">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Qualification:</label>
+            <input type="text" class="form-control" name="qual">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Year Passout:</label>
+            <input type="text" class="form-control" name="year">
+          </div>
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" name="applyFor" class="btn btn-primary">Apply</button>
+      </div>
+      </form>
     </div>
   </div>
 </div>
